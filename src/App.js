834a34f54078dbccaac4d70a7de05c598b38d886
seshu-dev <seshudev23@gmail.com>
2023-04-17 20:47:43 +0530

    task1 update

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect } from 'react';
+import Table from './components/Table';
+
+function App() {
+
+  
+
+  return (
+    <div className="App">
+    <Table/>
+    </div>
+  );
+}
+
+export default App;
