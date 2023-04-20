fae497f9df51f75aa2865e75bd4c437ab380194b
seshu-dev <seshudev23@gmail.com>
2023-04-20 11:58:08 +0530

    omitted the useState

--- src/base/reducer/tableReducer.js
+++ src/base/reducer/tableReducer.js
@@ -1,21 +1,28 @@
 import { createSlice } from "@reduxjs/toolkit"
 
 const initialState = {
-    tableData: []
+    mainTableData: [],
+    tableShowData:[],
+    searchTableData:[]
 }
 
 const tabledataSlice = createSlice({
     name: "TableArray",
     initialState,
     reducers: {
-        tableData: (state, action) => {
-            state.tableData=(action.payload)
+        mainTableDataAction: (state, action) => {
+            state.mainTableData=(action.payload)
         },
-
+        tableShowAction:(state,action)=>{
+            state.tableShowData=(action.payload)
+        },
+        searchTableDataAction:(state,action)=>{
+            state.searchTableData=(action.payload)
+        }
     }
 
 
 })
 export default tabledataSlice.reducer
-export const { tableData } = tabledataSlice.actions
+export const { mainTableDataAction ,tableShowAction, searchTableDataAction } = tabledataSlice.actions
 
--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -1,77 +1,77 @@
 import axios from 'axios'
-import React, { useEffect, useReducer, useState } from 'react'
+import React, { useEffect, useReducer } from 'react'
 import 'bootstrap/dist/css/bootstrap.css';
 import "./table.css"
-import { tableData } from '../../base/reducer/tableReducer';
+import { mainTableDataAction, searchTableDataAction, tableShowAction } from '../../base/reducer/tableReducer';
 import { useDispatch, useSelector } from "react-redux"
 
 
 function Table() {
     const Reduxdispatch = useDispatch();
-    const ReduxTableData = useSelector((state) => state.tableDataReducer.tableData);
+    const { mainTableData, tableShowData, searchTableData } = useSelector((state) => state.tableDataReducer);
     function reducer(state, action) {
         switch (action.type) {
             case "Increment":
                 return { pageNumber: state.pageNumber + 1 }
             case "Decrement":
                 return { pageNumber: state.pageNumber - 1 }
+            default:
+                return { pageNumber: state.pageNumber }
+
         }
     }
     const [state, dispatch] = useReducer(reducer, { pageNumber: 1 })
-    const [tableValues, setTableValues] = useState([]);
-    const [searchTableValues, setSearchTableValues] = useState([]);
-    const tableDataReducer = useSelector((state) => state);
     useEffect(() => {
         doCallAnApi()
     }, [])
     const doCallAnApi = async () => {
         const result = await axios.get(`https://api.punkapi.com/v2/beers?page=${state.pageNumber}&per_page=10`)
-        setTableValues(result.data);
-        setSearchTableValues(result.data);
-        Reduxdispatch(tableData(result.data))
+        Reduxdispatch(tableShowAction(result.data))
+        Reduxdispatch(searchTableDataAction(result.data))
+        Reduxdispatch(mainTableDataAction(result.data))
 
     }
     const doSearchTheData = async (e) => {
-        const searchResp = await searchTableValues.filter((list) => {
+        const searchResp = await searchTableData.filter((list) => {
             return list.name.toLowerCase().includes(e.target.value.toLowerCase())
         })
-        setTableValues(searchResp)
+        Reduxdispatch(tableShowAction(searchResp))
     }
     const doDecrement = async () => {
         dispatch({ type: "Decrement" })
-        if (ReduxTableData.length / 10 >= state.pageNumber) {
+        if (mainTableData.length / 10 >= state.pageNumber) {
             let minusedStatePageNumber = state.pageNumber - 2
-            let arr = [...ReduxTableData]
+            let arr = [...mainTableData]
             const splice = await arr.splice(minusedStatePageNumber * 10, 10)
-            Reduxdispatch(tableData(arr))
-            setTableValues(splice);
-            setSearchTableValues(splice);
+            Reduxdispatch(mainTableDataAction(arr))
+            Reduxdispatch(tableShowAction(splice))
+            Reduxdispatch(searchTableDataAction(splice))
             if (minusedStatePageNumber == 0) {
-                Reduxdispatch(tableData(splice))
+                Reduxdispatch(mainTableDataAction(splice))
             }
         }
         else {
             const result = await axios.get(`https://api.punkapi.com/v2/beers?page=${state.pageNumber - 1}&per_page=10`)
-            setTableValues(result.data);
-            setSearchTableValues(result.data);
+            Reduxdispatch(tableShowAction(result.data))
+            Reduxdispatch(searchTableDataAction(result.data))
         }
     }
     const doIncrement = async () => {
         dispatch({ type: "Increment" })
         const result = await axios.get(`https://api.punkapi.com/v2/beers?page=${state.pageNumber + 1}&per_page=10`)
-        setTableValues(result.data);
-        setSearchTableValues(result.data);
-        if (ReduxTableData.length == 0) {
-            Reduxdispatch(tableData(result.data))
+        Reduxdispatch(tableShowAction(result.data))
+        Reduxdispatch(searchTableDataAction(result.data))
+        if (mainTableData.length == 0) {
+            Reduxdispatch(mainTableDataAction(result.data))
         } else {
             let arr = [];
-            ReduxTableData.forEach((list) => {
+            mainTableData.forEach((list) => {
                 arr.push(list)
             })
             result.data.forEach((list) => {
                 arr.push(list)
             })
-            Reduxdispatch(tableData(arr))
+            Reduxdispatch(mainTableDataAction(arr))
         }
     }
     return (
@@ -79,7 +79,7 @@ function Table() {
             <h1>Table list from Api page {state.pageNumber} </h1>
             <input placeholder='search...' onChange={doSearchTheData} />
             <div className='table' >
-                {tableValues.length == 0 ? <div><h3 className='noData' >No Data</h3></div> :
+                {tableShowData.length == 0 ? <div><h3 className='noData' >No Data</h3></div> :
                     <table className="table table-hover">
                         <thead>
                             <tr>
@@ -90,7 +90,7 @@ function Table() {
                             </tr>
                         </thead>
                         <tbody>
-                            {tableValues.map((datas, index) => {
+                            {tableShowData.map((datas, index) => {
                                 return (
                                     <tr key={index} >
                                         <th scope="row">{datas.id}</th>
